15a61ec4d39c8e196901b9d43a909f02c9aeaa19
rimanirfan <irfanriman@gmail.com>
2019-09-07 19:40:48 +0700

    added styling for currency list

--- src/components/CurrencyItem/CurrencyItem.js
+++ src/components/CurrencyItem/CurrencyItem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import './CurrencyItem.css';
+
 const CurrencyItem = ({amount, currency}) => {
     return (
-        <div>
-            <div>{currency.symbol} {currency.rate * amount}</div>
-            <div>{currency.symbol} - {currency.detail}</div>
-            <div>1 USD = {currency.symbol} {currency.rate}</div>
+        <div className="ui aligned centered grid currency-item">
+            <div className="fourteen wide column border-right">
+                <div className="ui grid">
+                    <div className="four wide column">{currency.symbol}</div>
+                    <div className="twelve wide column text-right">{currency.rate * amount}</div>
+                </div>
+                <div className="text-bold text-italic">{currency.symbol} - {currency.detail}</div>
+                <div className="text-italic">1 USD = {currency.symbol} {currency.rate}</div>
+            </div>
+            <div className="two wide middle aligned column">(-)</div>
         </div>
     );
 };
